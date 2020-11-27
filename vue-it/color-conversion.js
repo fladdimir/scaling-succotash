bbb014d92b8ca03c45b8732e1cddd349766293d0
fladdimir <fladdi.mir@gmx.de>
2020-11-27 22:37:08 +0100

    add fetch from db

--- vue-it/color-conversion.js
+++ vue-it/color-conversion.js
@@ -1,3 +1,45 @@
+function toHsl(r, g, b) {
+    r = parseFloat(r);
+    g = parseFloat(g);
+    b = parseFloat(b);
+    if (r < 0) r = 0;
+    if (g < 0) g = 0;
+    if (b < 0) b = 0;
+    if (r > 255) r = 255;
+    if (g > 255) g = 255;
+    if (b > 255) b = 255;
+    hex = r * 65536 + g * 256 + b;
+    hex = hex.toString(16, 6);
+    len = hex.length;
+    if (len < 6)
+        for (i = 0; i < 6 - len; i++)
+            hex = '0' + hex;
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    M = Math.max(r, g, b);
+    m = Math.min(r, g, b);
+    d = M - m;
+    if (d == 0) h = 0;
+    else if (M == r) h = ((g - b) / d) % 6;
+    else if (M == g) h = (b - r) / d + 2;
+    else h = (r - g) / d + 4;
+    h *= 60;
+    if (h < 0) h += 360;
+    l = (M + m) / 2;
+    if (d == 0)
+        s = 0;
+    else
+        s = d / (1 - Math.abs(2 * l - 1));
+    s *= 100;
+    l *= 100;
+    h = h.toFixed(0);
+    s = s.toFixed(1);
+    l = l.toFixed(1);
+    return [h, s, l];
+}
+
+
 function toRgb(h, s, l) {
     h = parseFloat(h);
     s = parseFloat(s);
