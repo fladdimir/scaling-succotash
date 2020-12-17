fb87bc6c9d433f53bc718624caab2a31442f308c
fladdimir <fladdi.mir@gmx.de>
2020-12-17 17:32:42 +0100

    springboot: add handler config for serving static files from directory

---
+++ springboot/src/main/java/succotash/MvcConfig.java
@@ -0,0 +1,15 @@
+package succotash;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/vue-it/**").addResourceLocations("file:../vue-it/");
+    }
+}
